2ff450db929ca2376dbf32c112fa71a274f8a06b
Lyotaaa <korystin.aleksei@yandex.ru>
2023-09-26 16:04:25 +0300

    add new files

--- app/config_db.py
+++ app/config_db.py
@@ -1,4 +1,8 @@
 import environ
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 
 env = environ.Env()
 environ.Env.read_env()
@@ -8,4 +12,9 @@ POSTGRES_DB = env("POSTGRES_DB")
 DB_HOST = env("DB_HOST")
 DB_PORT = env("DB_PORT")
 
-PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
+PG_DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
+
+engine = create_async_engine(PG_DSN)
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
---
+++ app/server.py
@@ -0,0 +1,4 @@
+import typing
+import json
+from models import Acync_AdsModel, Acync_OwnerModel
+import asyncio
